145b223f9c4e2f37c3a27b6272ed9d9ef10fa184
Satwik Kaushik <satwikkaushik@worlspace.me>
2024-01-15 23:28:14 +0530

    Updated UI of mainView
    Added UI till Side Menu. No change in code. App is working fine, no warnings.

--- src/main/java/com/tasks/Main.java
+++ src/main/java/com/tasks/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,8 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("mainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.initStyle(StageStyle.UNDECORATED);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
         stage.setTitle("Tasks");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
